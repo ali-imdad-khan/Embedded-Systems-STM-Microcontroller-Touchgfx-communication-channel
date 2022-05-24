ad1f7b573180069f24caaea911bb88cec54e8b38
Ali Imdad Khan <105221953+ali-imdad-khan@users.noreply.github.com>
2022-05-24 18:01:31 +0200

    Update lufthansa_homePresenter.cpp

--- TouchGFX/gui/src/lufthansa_home_screen/lufthansa_homePresenter.cpp
+++ TouchGFX/gui/src/lufthansa_home_screen/lufthansa_homePresenter.cpp
@@ -1,1 +1,36 @@
-asd
+lufthansa_home
+#include <gui/lufthansa_home_screen_screen/lufthansa_home_screenView.hpp>
+#include <gui/lufthansa_home_screen_screen/lufthansa_home_screenPresenter.hpp>
+
+lufthansa_home_screenPresenter::lufthansa_home_screenPresenter(lufthansa_home_screenView& v)
+    : view(v)
+{
+
+}
+
+void lufthansa_home_screenPresenter::activate()
+{
+
+}
+
+void lufthansa_home_screenPresenter::deactivate()
+{
+
+}
+
+void lufthansa_home_screenPresenter::setAirPressureSensor(double value)
+{
+	view.updateAirPressureSensor(value);
+}
+
+
+void lufthansa_home_screenPresenter::receive_Movie_FromQueue(){
+	model->queue_Read_Movie();
+}
+void lufthansa_home_screenPresenter::Put_Movie_ToQueue(){
+	model->queue_Put_Movie();
+}
+
+
+
+
