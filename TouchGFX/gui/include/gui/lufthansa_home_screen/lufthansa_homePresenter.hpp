1fe52e1a6740675f7a351f82f2231c14e0115aee
Ali Imdad Khan <105221953+ali-imdad-khan@users.noreply.github.com>
2022-05-24 17:39:16 +0200

    Update lufthansa_homePresenter.hpp

--- TouchGFX/gui/include/gui/lufthansa_home_screen/lufthansa_homePresenter.hpp
+++ TouchGFX/gui/include/gui/lufthansa_home_screen/lufthansa_homePresenter.hpp
@@ -1,1 +1,53 @@
-lufthansa_homePresenter.hpp
+#ifndef LUFTHANSA_HOME_SCREENPRESENTER_HPP
+#define LUFTHANSA_HOME_SCREENPRESENTER_HPP
+
+#include <gui/model/ModelListener.hpp>
+#include <mvp/Presenter.hpp>
+
+using namespace touchgfx;
+
+class lufthansa_home_screenView;
+
+class lufthansa_home_screenPresenter : public touchgfx::Presenter, public ModelListener
+{
+public:
+    lufthansa_home_screenPresenter(lufthansa_home_screenView& v);
+
+    /**
+     * The activate function is called automatically when this screen is "switched in"
+     * (ie. made active). Initialization logic can be placed here.
+     */
+    virtual void activate();
+
+    /**
+     * The deactivate function is called automatically when this screen is "switched out"
+     * (ie. made inactive). Teardown functionality can be placed here.
+     */
+    virtual void deactivate();
+
+    virtual ~lufthansa_home_screenPresenter() {};
+
+    //Movie Queue Read calls to model - > main
+    void receive_Movie_FromQueue();
+
+    //Receive Air Pressure periodically read value
+    void setAirPressure(double value);
+
+
+
+///Get Set methods to receive and set values in model data
+    int8_t getMovie(){
+    	return model->getModelMovie();
+    }
+
+    void setMovie(int8_t value){
+    	model->setModelMovie(value);
+    }
+
+private:
+    lufthansa_home_screenPresenter();
+
+    lufthansa_home_screenView& view;
+};
+
+#endif // LUFTHANSA_HOME_SCREENPRESENTER_HPP
