53643492aaf343abfcf55fc40983ffd82042776a
Ali Imdad Khan <105221953+ali-imdad-khan@users.noreply.github.com>
2022-05-24 17:42:25 +0200

    Update lufthansa_homeView.hpp

--- TouchGFX/gui/include/gui/lufthansa_home_screen/lufthansa_homeView.hpp
+++ TouchGFX/gui/include/gui/lufthansa_home_screen/lufthansa_homeView.hpp
@@ -1,1 +1,28 @@
-lufthansa_homePresenter.hpp
+
+#ifndef LUFTHANSA_HOME_SCREENVIEW_HPP
+#define LUFTHANSA_HOME_SCREENVIEW_HPP
+
+#include <gui_generated/lufthansa_home_screen_screen/lufthansa_home_screenViewBase.hpp>
+#include <gui/lufthansa_home_screen/lufthansa_home_screenPresenter.hpp>
+
+class lufthansa_home_screenView : public lufthansa_home_screenViewBase
+{
+public:
+    lufthansa_home_screenView();
+    virtual ~lufthansa_home_screenView() {}
+    virtual void setupScreen();
+    virtual void tearDownScreen();
+
+    void updateAirPressureSensor(double value);
+
+    void updateMovieSelection(int8_t value);
+
+
+protected:
+
+    int8_t movie_id;
+
+
+};
+
+#endif // LUFTHANSA_HOME_SCREENVIEW_HPP
